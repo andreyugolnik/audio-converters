ab2c9aef9076a9deb76b1a7e66c2406703c1a3c1
Andrey A. Ugolnik <andrey@ugolnik.info>
2018-03-13 02:49:37 +0300

    Tracker (xm, s3m, etc) to MP3 and OGG converter added.

---
+++ convert_tracker_mp3_ogg.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# convert all input file to MP3 and 1ch_44KHz_OGG.
+
+if test -f "$1"
+then
+    input_file="$1"
+    input_name="${input_file%.*}"
+    input_wav="${input_name}.wav"
+    mp3="${input_name}.mp3"
+    ogg="${input_name}.ogg"
+
+    rm -f "${mp3}" "${ogg}" "${input_wav}"
+
+    xmp "${input_file}" -o "${input_wav}"
+
+    ffmpeg -i "${input_wav}" -acodec mp3 "${mp3}"
+
+    oggenc --downmix "${input_wav}" -o "${ogg}"
+else
+    echo "No input file provided."
+fi
